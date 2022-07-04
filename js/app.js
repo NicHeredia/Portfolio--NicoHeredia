8ab7d36fb62a243d4538ee4a054c8b34dd449fee
Nicolas Heredia <nicoherediasp@gmail.com>
2022-07-03 23:17:41 -0300

    Merge branch 'master' of https://github.com/NicHeredia/Challenge-3---Portfolio

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+     //reset: true
+});
+
+sr.reveal(".inicio-contenedor-titulos, .sobre-mi__contenedor, .boton-curriculum, .conocimientos-contenedor, .contenedor-cursos, .card-proyecto, .contenedor-contacto, .contacto-descripcion, .footer-descripcion", {delay: 400});
+
+sr.reveal(".titulo-section");
+
